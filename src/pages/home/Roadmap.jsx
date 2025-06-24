a9cc7ef334231aa0c37a23da76b0897c27d4f51e
Sajmul <sajmul1427@gmail.com>
2025-06-24 23:46:50 +0600

    local storage setup done

--- src/pages/home/Roadmap.jsx
+++ src/pages/home/Roadmap.jsx
@@ -6,49 +6,58 @@ import Loading from "../../components/Loading";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { Link } from "react-router";
+import { FaCommentAlt } from "react-icons/fa";
 
 const Roadmap = ({ roadmap }) => {
   const queryClient = useQueryClient();
-  const {user} = useAuth();
+  const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const btnRef = useRef();
+  const btnRef = useRef(null);
 
+  const { title, category, description, status, upvotes, _id } = roadmap || {};
 
-  const { title, category, description, status, upvotes,_id } = roadmap || {};
-
-  const {mutateAsync, isPending} = useMutation({
-    mutationKey: ['vote', user.email],
-    mutationFn: async() => {
+  const { mutateAsync, isPending } = useMutation({
+    mutationKey: ["vote", user.email],
+    mutationFn: async () => {
       const { data } = await axiosSecure.post(`/roadmaps/vote/${_id}`, {
         email: user.email,
       });
       toast.success(data?.message);
       btnRef.current.disabled = true;
-      queryClient.invalidateQueries({queryKey: ["roadmaps"]})
+      queryClient.invalidateQueries({ queryKey: ["roadmaps"] });
     },
-    onError: err => {
-      toast.error(err.response.data.message || 'Something went wrong')
-    }
-  })
-  const handleVote = async () =>  {
+    onError: (err) => {
+      toast.error(err.response.data.message || "Something went wrong");
+    },
+  });
+  const handleVote = async () => {
     await mutateAsync();
-  }
-  const isVoted= upvotes.find(vote => vote.user === user?.email);
+  };
+
+  const isVoted = upvotes.find((vote) => vote.user === user?.email);
 
   return (
-    <Link to={`/roadmap/${_id}`} className="rounded-md border p-4 flex flex-col justify-between">
+    <Link
+      to={`/roadmap/${_id}`}
+      className="rounded-md border p-4 flex flex-col justify-between"
+    >
       <h3 className="font-bold text-2xl mb-4">{title}</h3>
       {description && <p className="mb-3">{description}</p>}
 
       <div className="flex items-center justify-between">
         <p className="flex items-center gap-4">
-            <span className="bg-sec w-6 h-6 rounded-full flex-center">{upvotes.length}</span>
+          <span className="bg-sec w-6 h-6 rounded-full flex-center">
+            {upvotes.length}
+          </span>
           <button ref={btnRef} disabled={!!isVoted} onClick={handleVote}>
-            {
-              isPending ? <Loading /> : <MdHowToVote size={30} />
-            }
+            {isPending ? <Loading /> : <MdHowToVote size={30} />}
           </button>
         </p>
+
+        <button>
+          <FaCommentAlt size={28} className="text-main" />
+        </button>
+
         <div className="flex gap-4 items-center">
           <p className="capitalize bg-main w-fit px-3 py-0.5 text-white rounded-full">
             {category}
--- src/pages/home/RoadmapDetails.jsx
+++ src/pages/home/RoadmapDetails.jsx
@@ -1,11 +1,44 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import { FaCommentAlt } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
+import { MdHowToVote } from "react-icons/md";
 import { useParams } from "react-router";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
 import Loading from "../../components/Loading";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const RoadmapDetails = () => {
   const { id } = useParams();
   const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const btnRef = useRef(null);
+  const queryClient = useQueryClient();
+  const [open, setOpen] = useState(JSON.parse(localStorage.getItem("isOpen")));
+  const sendRef = useRef(null);
+
+  const { mutateAsync: postComment, isPending: isCommenting } = useMutation({
+    mutationKey: ["comments"],
+    mutationFn: async (body) => {
+      await axiosSecure.post("/comments", body);
+      queryClient.invalidateQueries({ queryKey: ["comments"] });
+    },
+    onError: (error) => {
+      toast.error(
+        error?.response.data.message || "Something went wrong while commenting"
+      );
+    },
+  });
+
+  const { data: comments = [], isLoading: commentComing } = useQuery({
+    queryKey: ["comments"],
+    enabled: !!open,
+    queryFn: async () => {
+      const { data } = await axiosSecure("/comments");
+      return data.data || [];
+    },
+  });
 
   const { data: roadmap = {}, isLoading } = useQuery({
     queryKey: ["roadmap", id],
@@ -15,7 +48,34 @@ const RoadmapDetails = () => {
     },
   });
 
-  const { title, description, status, category, upvotes}  = roadmap || {};
+  const {
+    title,
+    description,
+    status,
+    category,
+    upvotes = [],
+    _id = [],
+  } = roadmap || {};
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationKey: ["vote", user.email],
+    mutationFn: async () => {
+      const { data } = await axiosSecure.post(`/roadmaps/vote/${_id}`, {
+        email: user.email,
+      });
+      toast.success(data?.message);
+      btnRef.current.disabled = true;
+      queryClient.invalidateQueries({ queryKey: ["roadmaps"] });
+    },
+    onError: (err) => {
+      toast.error(err.response.data.message || "Something went wrong");
+    },
+  });
+  const handleVote = async () => {
+    await mutateAsync();
+  };
+
+  const isVoted = upvotes.find((vote) => vote.user === user?.email);
 
   if (isLoading) {
     return (
@@ -25,11 +85,86 @@ const RoadmapDetails = () => {
     );
   }
 
+  const handleChange = (e) => {
+    if (e.target.value.length === 0) {
+      sendRef.current.disabled = true;
+    } else {
+      sendRef.current.disabled = false;
+    }
+  };
+
   return (
     <section className="section">
       <div className="max-w-2xl mx-auto border p-6 rounded-md">
         <h3 className="font-semibold">{title}</h3>
+        {description && <p className="mt-4">{description}</p>}
 
+        <div className="flex items-center justify-between mt-4">
+          <p className="flex items-center gap-4">
+            <span className="bg-sec w-6 h-6 rounded-full flex-center">
+              {upvotes.length}
+            </span>
+            <button ref={btnRef} disabled={!!isVoted} onClick={handleVote}>
+              {isPending ? <Loading /> : <MdHowToVote size={30} />}
+            </button>
+          </p>
+
+          <button
+            title="Click to open or close comments"
+            onClick={() => {
+              setOpen(!open);
+              localStorage.setItem("isOpen", !open);
+            }}
+          >
+            <FaCommentAlt size={28} className="text-main" />
+          </button>
+
+          <div className="flex gap-4 items-center">
+            <p className="capitalize bg-main w-fit px-3 py-0.5 text-white rounded-full">
+              {category}
+            </p>
+            <p
+              className={`capitalize bg-${
+                status === "planned"
+                  ? "amber-700"
+                  : status === "in_progress"
+                  ? "blue-700"
+                  : "sec"
+              } w-fit px-3 py-0.5 text-white rounded-full`}
+            >
+              {status === "in_progress" ? "In Progress" : status}
+            </p>
+          </div>
+        </div>
+
+        {/* comment section */}
+        <hr className="my-6" />
+        <div>
+          <div className="flex justify-between">
+            <input
+              className="input rounded-r-none"
+              type="text"
+              name="comment"
+              placeholder="Leave a comment"
+              onChange={handleChange}
+            />
+            <button ref={sendRef} disabled className="btn rounded-l-none">
+              Send <IoSend />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`max-w-2xl mx-auto overflow-hidden border rounded-md mt-4 ${
+          open ? "h-fit block" : "h-0 hidden"
+        }`}
+      >
+        <div className="p-6">
+          <h2 className="font-bold text-2xl">
+            Comments: {comments.length || 0}
+          </h2>
+        </div>
       </div>
     </section>
   );
