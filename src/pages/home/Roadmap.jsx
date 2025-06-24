833b5549d4117cf9ba91c4e3e5b33852be89f35c
Sajmul <sajmul1427@gmail.com>
2025-06-24 18:22:45 +0600

    successfully vote done and button disable after vote

--- src/pages/home/Roadmap.jsx
+++ src/pages/home/Roadmap.jsx
@@ -1,7 +1,37 @@
 import { MdHowToVote } from "react-icons/md";
+import useAuth from "../../hooks/useAuth";
+import { useMutation } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Loading from "../../components/Loading";
+import toast from "react-hot-toast";
+import { useRef } from "react";
 
 const Roadmap = ({ roadmap }) => {
-  const { title, category, description, status, upvotes } = roadmap || {};
+  const {user} = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const btnRef = useRef();
+
+
+  const { title, category, description, status, upvotes,_id } = roadmap || {};
+
+  const {mutateAsync, isPending} = useMutation({
+    mutationKey: ['vote', user.email],
+    mutationFn: async() => {
+      const { data } = await axiosSecure.post(`/roadmaps/vote/${_id}`, {
+        email: user.email,
+      });
+      toast.success(data?.message);
+      btnRef.current.disabled = true;
+    },
+    onError: err => {
+      toast.error(err.response.data.message || 'Something went wrong')
+    }
+  })
+  const handleVote = async () =>  {
+    await mutateAsync();
+  }
+  const isVoted= upvotes.find(vote => vote.user === user?.email);
+
   return (
     <div className="rounded-md border p-4 flex flex-col justify-between">
       <h3 className="font-bold text-2xl mb-4">{title}</h3>
@@ -9,8 +39,10 @@ const Roadmap = ({ roadmap }) => {
 
       <div className="flex items-center justify-between">
         <p className="flex items-center gap-4">
-          <button>
-            <MdHowToVote size={30} />
+          <button ref={btnRef} disabled={!!isVoted} onClick={handleVote}>
+            {
+              isPending ? <Loading /> : <MdHowToVote size={30} />
+            }
           </button>
           <span className="bg-sec w-6 h-6 rounded-full flex-center">{upvotes.length}</span>
         </p>
