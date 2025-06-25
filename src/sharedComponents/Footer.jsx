827671ae340caa9211af7945647c2346ad54ddca
Sajmul <sajmul1427@gmail.com>
2025-06-25 19:21:25 +0600

    simple footer added

--- src/sharedComponents/Footer.jsx
+++ src/sharedComponents/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            footer
+        <footer className='bg-main'>
+            <section className="section my-0 flex justify-between items-center text-white">
+                <h3 className='font-bold text-2xl'>Roadmap App</h3>
+                <p>{new Date().getFullYear()}. All Rights Reserved</p>
+            </section>
         </footer>
     );
 };
